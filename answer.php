7ec7d5c4ffe5057482942da4ad3458189d3789ff
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-14 15:56:08 +0000

    edited php

--- answer.php
+++ answer.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Area of a Trapezoid, in PHP" />
+  <meta name="description" content="Volume of a Triangle, in PHP" />
   <meta name="keywords" content="mths, ics2o" />
   <meta name="author" content="Nathan De Silva" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -13,49 +14,49 @@
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Area of a Trapezoid, in PHP</title>
+  <title>Volume of a Pyramid, in PHP</title>
 </head>
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Area of a Trapezoid, in PHP</span>
+        <span class="mdl-layout-title">Volume of a Pyramid, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/trapezoid.svg" alt="trapezoid" width="250" />
+        <img src="./images/pyramid (1).png" alt="pyramid" width="250" />
       </div>
       <div class="page-content-php">
-        <div name="area-of-trapezoid">
+        <div name="volume-of-pyramid">
           <div class="mdl-layout__header-row">
             <br />
             <div class="mdl-layout__content">
             </div>
-            <div class= "mdl-layout__left">
-        <?php
+            <div class="mdl-layout__left">
+              <?php
 
-        // input
-        $aBase = $_POST["a-base"];
-        $bBase = $_POST["b-base"];
-        $height = $_POST["height"];
+              // input
+              $length = $_POST["length"];
+              $width = $_POST["width"];
+              $height = $_POST["height"];
 
-        // process
-        $area = (($aBase + $bBase) / 2) * $height;
+              // process
+              $volume = ($length * $width * $height) / 3;
 
-        // output
-        echo "If Base A is " . $aBase . " and Base B is " . $bBase . " and the height is " . $height . ". ";
-        echo "<br />";
-        echo "<br />";
-        echo "Then the Area is: " . $area . " mm²";
-        echo "<br />";
+              // output
+              echo "If the length is " . $length . " and the width is " . $width . " and the height is " . $height . ". ";
+              echo "<br />";
+              echo "<br />";
+              echo "Then the Volume is: " . $volume . " mm³";
+              echo "<br />";
 
-        ?>
+              ?>
+            </div>
+          </div>
         </div>
-      </div>
-  </div>
-  </main>
+    </main>
   </div>
 </body>
 </html>
